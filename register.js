b365bdfae216828d87c08a27b87ae101c288bc41
Leonardo Mamede Rodrigues <leonardomamedeee@gmail.com>
2021-10-17 18:51:16 -0300

    JAVASCRIPT WORKING HARD

--- register.js
+++ register.js
@@ -1,3 +1,123 @@
 onload = () =>{
-    console.log("javascript implementado")
+
+document.getElementById('btn').disabled = true;
+  
+//VERIFICAÇÃO EM TEMPO REAL
+
+nome.oninput = () => {
+//qualquer minima alteração no campo textual ele já responde (oninput)
+
+ //Transforma toda a entrada de letras para minuscula
+  nome.value = nome.value.toLowerCase();
+
+ //verificando a utilização de caracters nos campos textuais
+
+   if ('!@#$%ˆ&*()/+:.]ªº°[],;^~`´`|¹²³££¢¬¬¬¬"{}?*_=-¨'.indexOf(nome.value.charAt(nome.value.length - 1)) >= 0) 
+   {
+     //substr método de remoção de elementos de uma string. Se tiver algum caracter ele vai remover do valor
+     nome.value = nome.value.substr(0, nome.value.length - 1);
+   }
+
+   if ('0123456789'.indexOf(nome.value.charAt(nome.value.length - 1)) >= 0) 
+   {
+     //substr método de remoção de elementos de uma string. Se tiver algum caracter ele vai remover do valor
+     nome.value = nome.value.substr(0, nome.value.length - 1);
+   }
+ 
+  }
+
+//tratamento campo CPF
+  cpf.oninput = () =>{
+
+    cpf.value = cpf.value.toLowerCase();
+    
+    if('|!@#$%ˆ&*`´()+~ªº°[]^:.,|"¹²³££¢¬¬¬¬{}?/^~*_=-;¨abcdefghijklmnopçqrswtuvxyz'.indexOf(cpf.value.charAt(cpf.value.length - 1)) >=0 ){
+    
+      cpf.value = cpf.value.substr(0, cpf.value.length - 1);
+    }
+    
+
+
+  }
+//tratamento campo CEP
+  cep.oninput = () =>{
+
+    cep.value = cep.value.toLowerCase();
+    
+    if('|!@#$%ˆ&*`ªº´()+~°[]^:.,¹²³££¢¬¬¬¬|"{}?/^~*_=-;¨abcdefçghijklmnopqrswtuvxyz'.indexOf(cep.value.charAt(cep.value.length - 1)) >=0 ){
+    
+      cep.value = cep.value.substr(0, cep.value.length - 1);
+    }
+    
+
+
+  }
+//tratamento campo CELL
+  celular.oninput = () =>{
+
+    celular.value = celular.value.toLowerCase();
+    
+    if('|!@`´#$%ˆ&*()ªº°+~[]^:.,¹²³££¢¬¬¬¬|"{}?/^~*_=-;¨abcdefwgçhijklmnopqrstuvxyz'.indexOf(celular.value.charAt(celular.value.length - 1)) >=0 ){
+    
+      celular.value = celular.value.substr(0, celular.value.length - 1);
+    }
+    
+
+
+  }
+
+  
+//TRATAMENTO CAMPO SENHA
+senhaCadastro.onchange = ()=>{
+  if(senhaRepeat.value.length <8){
+    alert('Senha deve conter no minimo 8 caracteres');
+  }
 }
+
+  senhaRepeat.onchange = () =>{
+
+    if(senhaRepeat.value == senhaCadastro.value){
+      return true;
+    }
+
+    else{
+      alert("As senhas devem ser iguais!!");
+      
+    }
+
+  }
+
+
+  //CHECKBOX para controles de envio
+  concordaComTermos.onchange = ()=>{
+  console.log(concordaComTermos.checked);
+
+    if(concordaComTermos.checked == true){
+      document.getElementById('btn').disabled = false;
+    }
+    else{
+      document.getElementById('btn').disabled = true;
+    }
+  }
+
+ //verificação de envio 
+  cadastroFormulario.onsubmit = () =>{
+  
+    if(senhaRepeat.value == senhaCadastro.value && senhaRepeat.value.length >=8 ){
+      return true;
+    }
+
+    else{
+
+      alert("As senhas devem ser iguais!");
+      
+      return false;      
+     
+    }
+
+  }
+
+}
+
+
+
