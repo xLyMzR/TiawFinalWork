33c417885199aadce1657a292c6f83d485a676b8
Leonardo Mamede Rodrigues <leonardomamedeee@gmail.com>
2021-10-21 13:21:13 -0300

    JS

--- js/login.js
+++ js/login.js
@@ -2,7 +2,10 @@ onload = () =>{
 
 //VERIFICAÇÃO DO USUARIO CADASTRADO PARA LOGIN
     formularioLogin.onsubmit = ()=>{
-
+        if(email.value == '' || senha.value == ''){
+            return false;
+            
+        }
     if(email.value !== localStorage.getItem('emailLogin') && senha.value !== localStorage.getItem('senha')){
         
         return false;
@@ -10,10 +13,7 @@ onload = () =>{
     else{
        return true;
     }
-    if(email.value == '' || senha.value == ''){
-        return false;
-        
-    }
+  
 }
 
 }
