302f67fe2dd0a137c9fee846221797d2b9377195
Leonardo Mamede Rodrigues <leonardomamedeee@gmail.com>
2021-10-24 14:43:57 -0300

    verificação de campos vazios no formulario de login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,26 @@
 onload = () =>{
+//Verificação de campo vazio
+formularioLogin.onchange = ()=> {
+
+let campovazio = document.getElementById('campovazio');
+
+    if(email.value == ''){
+       
+        campovazio.innerHTML = "Campo obrigatório!";
+        campovazio.style.color = "orange";
+        campovazio.style.fontSize = "0.7em";
+
+        
+    }
+    else{campovazio.innerHTML = '';}
+
+    if(senha.value == ''){
+        campovazioSenha.innerHTML = "Campo obrigatório!";
+        campovazioSenha.style.color = "orange";
+        campovazioSenha.style.fontSize = "0.7em";
+    }
+    else{campovazioSenha.innerHTML = ''}
+}
 
 //VERIFICAÇÃO DO USUARIO CADASTRADO PARA LOGIN
     formularioLogin.onsubmit = ()=>{
