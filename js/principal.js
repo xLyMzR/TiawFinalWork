e493d6180bedb94bc6d02f0413d26ab5949552d2
Leonardo Mamede Rodrigues <leonardomamedeee@gmail.com>
2021-11-06 20:33:57 -0300

    WELCOME MSG SET

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,12 @@
 onload = () =>{
 
 
-
+    //CARREGA O NOME DO USUARIO CADASTRADO SALVO NO LOCALSTORAGE E EXIBE EM MENSAGEM DE BOAS VINDAS
     document.getElementById('welcomeMsg').innerHTML = localStorage.getItem('nomeUsuario');
     document.getElementById('welcomeMsg').style.textDecoration = 'underline';
     
 
-
+    //CRIANDO OBJETO PARA PODER PREENCHER OS CARDS DE SERVIÇO
 
 
 }
