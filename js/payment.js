cd231dbd16893a78dd216c7123acabc77a44740f
Leonardo Mamede Rodrigues <leonardomamedeee@gmail.com>
2021-11-09 20:08:04 -0300

    IMPLEMENT BUTTON FOR PAYMENT INFORMATIONS AND REGISTER

--- js/payment.js
+++ js/payment.js
@@ -1,7 +1,48 @@
-onload = () =>{
+onload = () => {
+  document.getElementById("cardsButtonInfo").onclick = () => {
+
+    document.getElementById("boxAndTextForInfoPaymentArea").innerHTML =
+      "VOCÊ NÃO POSSUI CARTÃO CADASTRADO";
+
+    document.getElementById("informationPaymentArea").style =
+      " border: 50px solid orange; ";
+
+    document.getElementById("addPaymentMethodArea").style.display = "flex";
+    document.getElementById("addPaymentMethodArea").style.flexDirection = "row";
+    document.getElementById("addPaymentMethodArea").style.justifyContent =
+      "center";
+    document.getElementById("addPaymentMethodBtn").innerHTML = "ADD CARD";
+  };
+
+  document.getElementById("paypalButtonInfo").onclick = () => {
+
+    document.getElementById("boxAndTextForInfoPaymentArea").innerHTML =
+      "VOCÊ NÃO POSSUI PAYPAL CADASTRADO";
+
+    document.getElementById("informationPaymentArea").style =
+      " border: 50px solid orange; ";
+
+    document.getElementById("addPaymentMethodArea").style.display = "flex";
+    document.getElementById("addPaymentMethodArea").style.flexDirection = "row";
+    document.getElementById("addPaymentMethodArea").style.justifyContent =
+      "center";
+    document.getElementById("addPaymentMethodBtn").innerHTML = "ADD ACCOUNT";
+  };
+
+  document.getElementById("pixButtonInfo").onclick = () => {
+
+    document.getElementById("boxAndTextForInfoPaymentArea").innerHTML =
+      "VOCÊ NÃO POSSUI CONTA PIX CADASTRADA";
+
+    document.getElementById("informationPaymentArea").style =
+      " border: 50px solid orange; ";
+
+    document.getElementById("addPaymentMethodArea").style.display = "flex";
+    document.getElementById("addPaymentMethodArea").style.flexDirection = "row";
+    document.getElementById("addPaymentMethodArea").style.justifyContent =
+      "center";
+    document.getElementById("addPaymentMethodBtn").innerHTML = "ADD ACCOUNT";
+  };
 
 
-
-
-    
-}
+};
